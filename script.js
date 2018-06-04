2732fc15b6259d05af2991d3965a10fa4df06485
tomas <perezgantomas@gmail.com>
2018-06-04 17:25:03 -0400

    fully functional exports and request

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-const array = require("./script.js");
 
-function random_item(q)
-{
-
-return q[Math.floor(Math.random()*q.length)];
-
-}
-let q = [
+let newQuotes =
+  function(i){
+  let morequotes=
+  [
   "Don't cry because it's over, smile because it happened.",
   "Be yourself; everyone else is already taken.",
   "Be the change that you wish to see in the world.",
   "No one can make you feel inferior without your consent.",
   "You only live once, but if you do it right, once is enough."
   ];
-
-console.log(random_item(q));
-module.exports.random_item = random_item;
-
-
-
-//this will console.log(everything)
-// for (let i = 0; i < q.length; i++) {
-//     console.log(q[i]);
-//   }
+  return morequotes[i];
+}
+module.exports.qs = newQuotes;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const http = require("http");
+let q = require("./script.js");
+let quotes = [Math.floor(Math.random()*4)];
+console.log(q.qs(quotes));
+
 http.createServer((request, response) => {
 response.writeHead(200, { "Content-type": "text/plain" });
-response.write("sup, World");
+response.write("sup, World!");
+response.write(q.qs(quotes));
 response.end();
 }).listen(3000);
